12450dcb3041c597ec4c9f58f709a792d3d9b4bf
Michael-J-Fernandez <michael.fernandez@codeimmersives.com>
2022-09-19 10:55:11 -0400

    Finish logic for excluding all invalid inputs.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,38 +3,22 @@ const prompt = require('prompt-sync')({sigint: true});
 let player1 = prompt("Player 1: Choose rock, paper, or scissors: ")
 let player2 = prompt("Player 2: Choose rock, paper, or scissors: ")
 
-if (player1 === player2) {
-    console.log("Draw!");
-} else if (player1 === "rock") {
-    if (player2 === "paper") {
+if ((player1 === "rock" || player1 === "paper" || player1 === "scissors") && (player2 === "rock" || player2 === "paper" || player2 === "scissors")) {
+    if (player1 === player2) {
+        console.log("Draw!");
+    } else if (player1 === "rock" && player2 === "scissors") {
+        console.log("Player 1 wins!");
+    } else if (player1 === "rock" && player2 === "paper") {
         console.log("Player 2 wins!");
-    } else if (player2 === "scissors") {
-        console.log("Player 1 wins!")
-    }   
-} else if (player1 === "paper") {
-    if (player2 === "scissors") {
-        console.log("Player 2 wins!");
-    } else if (player2 === "rock") {
-        console.log("Player 1 wins!")
-    }
-} else if (player1 === "scissors") {
-    if (player2 === "rock") {
+    } else if (player1 === "paper" && player2 === "rock") {
+        console.log("Player 1 wins!");
+    } else if (player1 === "paper" && player2 === "scissors") {
         console.log("Player 2 wins!")
-    } else if (player2 === "paper") {
+    } else if (player1 === "scissors" && player2 === "paper") {
         console.log("Player 1 wins!")
-    }
+    } else if (player1 === "scissors" && player2 === "rock") {
+        console.log("Player 2 wins!")
+    } 
+} else {
+        console.log("Error: Invalid Entry")
 }
-
-/*
-"rock" = (> "scissors")
-"paper" = (> "rock")
-"scissors" = (> "paper")
-
-if (player1 === player2) {
-    console.log("Draw!")
-} else if (player1 > player2) {
-    console.log("Player 1 wins!")
-} else if (player1 < player2) {
-    console.log("Player 2 wins!");
-}
-*/
